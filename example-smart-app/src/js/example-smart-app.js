e90147ad2d31728f4ba96519bcea9a29cef02158
hgraybill3 <50804084+hgraybill3@users.noreply.github.com>
2019-10-18 19:59:54 -0400

    adding weight

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -61,6 +61,7 @@
 
           p.hdl = getQuantityValueAndUnit(hdl[0]);
           p.ldl = getQuantityValueAndUnit(ldl[0]);
+          p.weight = getQuantityValueAndUnit(weight[0]);
 
           ret.resolve(p);
         });
